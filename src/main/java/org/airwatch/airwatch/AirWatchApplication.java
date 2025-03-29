6b6a7e2130859971f59f2d53d9db79ec0cd33fa8
Said Cemal Türkdoğan <69085609+saidturkdogan@users.noreply.github.com>
2025-03-29 16:36:33 +0300

    inital commit

---
+++ src/main/java/org/airwatch/airwatch/AirWatchApplication.java
@@ -0,0 +1,13 @@
+package org.airwatch.airwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirWatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirWatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/airwatch/airwatch/AirWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.airwatch.airwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirWatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
